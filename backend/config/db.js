91042324d920d8de4c8975273c37737d8cdbe0c7
Mohammed imran <imranambassadorofislam@gmail.com>
2026-07-13 18:23:13 +0530

    Fourth commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/tailorpro');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://TailorPro:admin123@cluster0.opezko1.mongodb.net/?appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
