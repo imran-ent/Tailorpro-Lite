2443caaafb5f44cd168c40064e399985158fe286
Mohammed imran <imranambassadorofislam@gmail.com>
2026-07-13 18:45:53 +0530

    six commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'mongodb+srv://TailorPro:admin123@cluster0.opezko1.mongodb.net/?appName=Cluster0',
 });
 
 // Interceptor to inject JWT token in Authorization header
